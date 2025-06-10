772b3866ec2383f5a558f5f0ff8f420984a49d41
hiroro1204 <hiroro.hiroron@gmail.com>
2025-06-10 19:24:25 +0900

    ハンバーガーメニューのバグ修正2

--- js/component/scrollHeader.js
+++ js/component/scrollHeader.js
@@ -10,46 +10,12 @@ export const initializeScrollHeader = () => {
       ".js-header-scrollTarget"
     );
 
-    /**
-     *  headerNavを非表示にする
-     *  IntersectionObserverを使用して、スクロールしたらheaderNavを非表示にする
-     */
-
-    // IntersectionObserverのOption
-    // const observerOptions = {
-    //   root: null,
-    //   rootMargin: "0px",
-    //   threshold: 0,
-    // };
-
-    // const headerNavVisibilityHandler = (entries) => {
-    //   entries.forEach((entry) => {
-    //     // ターゲットの要素の画面外に出た時に実行
-    //     if (entry.isIntersecting) {
-    //       scrollObserver.unobserve(scrollTargetElement);
-    //     }
-    //   });
-    // };
-
-    // const scrollObserver = new IntersectionObserver(
-    //   headerNavVisibilityHandler,
-    //   observerOptions
-    // );
-
-    // scrollObserver.observe(scrollTargetElement);
-
-    /**
-     *  headerを固定にする
-     *  上方向へスクロールしたらheaderを表示し、下方向へスクロールしたらheaderを非表示にする
-     */
-
     // 現在のスクロール位置を取得
     let lastScrollY = window.scrollY;
 
     window.addEventListener("scroll", () => {
       if (window.scrollY < lastScrollY) {
         // 上方向へスクロールしたらheaderを見せる
-        console.log("上方向へスクロールしたらheaderを見せる");
         header.classList.remove("is-hidden");
         headerNav.classList.add("is-hidden");
       } else {
@@ -57,8 +23,6 @@ export const initializeScrollHeader = () => {
         header.classList.add("is-hidden");
       }
       lastScrollY = window.scrollY;
-      console.log(lastScrollY);
-      console.log(window.scrollY);
     });
   });
 };
